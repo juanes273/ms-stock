7ccfc6a23eafda0ddb18d47ef0839fa56ce675c1
juanes273 <75282431+juanes273@users.noreply.github.com>
2024-08-20 12:03:28 -0500

    First Commit

---
+++ src/main/java/com/bootcamp_2024/ms_stock/MsStockApplication.java
@@ -0,0 +1,14 @@
+package com.bootcamp_2024.ms_stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class MsStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcamp_2024/ms_stock/MsStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp_2024.ms_stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
