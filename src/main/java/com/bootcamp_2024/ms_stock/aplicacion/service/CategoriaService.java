b2c0e1c9673c95cc44048610096a93c384765c28
juanes273 <75282431+juanes273@users.noreply.github.com>
2024-08-22 12:41:58 -0500

    Ajustes de independencia

--- src/main/java/com/bootcamp_2024/ms_stock/aplicacion/service/CategoriaService.java
+++ src/main/java/com/bootcamp_2024/ms_stock/aplicacion/service/CategoriaService.java
@@ -3,20 +3,24 @@ package com.bootcamp_2024.ms_stock.aplicacion.service;
 
 import com.bootcamp_2024.ms_stock.aplicacion.excepciones.CategoriaAlreadyExistsException;
 import com.bootcamp_2024.ms_stock.dominio.modelo.Categoria;
-import com.bootcamp_2024.ms_stock.dominio.repository.CategoriaRepository;
+import com.bootcamp_2024.ms_stock.infraestructura.adaptadores.salida.persistencia.repository.CategoriaRepositoryAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoriaService {
 
+    private final CategoriaRepositoryAdapter categoriaRepositoryAdapter;
+
     @Autowired
-    private CategoriaRepository categoriaRepository;
+    public CategoriaService(CategoriaRepositoryAdapter categoriaRepositoryAdapter) {
+        this.categoriaRepositoryAdapter = categoriaRepositoryAdapter;
+    }
 
     public Categoria crearCategoria(Categoria categoria) {
-        if (categoriaRepository.existsByNombre(categoria.getNombre())) {
+        if (categoriaRepositoryAdapter.existsByNombre(categoria.getNombre())) {
             throw new CategoriaAlreadyExistsException("El nombre de la categoría ya existe: " + categoria.getNombre());
         }
-        return categoriaRepository.save(categoria);
+        return categoriaRepositoryAdapter.save(categoria);
     }
 }
